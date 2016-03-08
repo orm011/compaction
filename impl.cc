adf009eec238d9e6a1d3aa0e39609973f7e66e83
Oscar Moll <orm@csai.mit.edu>
2016-03-07 19:11:18 -0500

    vecsize... :O

--- impl.cc
+++ impl.cc
@@ -32,7 +32,7 @@ tuple<int,int,int> count_mask(int *d, int len, int lim1, int lim2) {
   const __m256i lim1_ = _mm256_set1_epi32(lim1);
   const __m256i lim2_ = _mm256_set1_epi32(lim2);
 
-  for (int i = 0; i < len; i+=sizeof(__m256i)) {
+  for (int i = 0; i < len; i+=k_vecsize) {
     auto data_ = _mm256_load_si256((__m256i*)&(d[i]));
     {
       auto tmpgt1_ = _mm256_cmpgt_epi32(data_, lim1_); // gt returns 0 or 0xffff..f
